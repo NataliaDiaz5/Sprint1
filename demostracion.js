355c62c83d3582d3cc177d799790d4395f9aefe1
natalia diaz <nd@oiga.com>
2021-10-06 20:42:01 -0500

    demostracion

---
+++ demostracion.js
@@ -0,0 +1,7 @@
+function filtrarPorContrasena(arreglo, filtro){
+
+    let res_filtro = array.filter(p => p.contrasena.length <= filtro);
+    console.log(res_filtro)
+    return res_filtro;
+   
+}
